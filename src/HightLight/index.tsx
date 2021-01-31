3d4fed9d941333f3d925da7e4291f95741269dc4
Aliaksei Mishyn <trouvere777@mail.ru>
2021-01-31 11:35:31 +0300

    Add key to HightLight

--- src/HightLight/index.tsx
+++ src/HightLight/index.tsx
@@ -1,38 +1,42 @@
-import React, { useState, FunctionComponent } from "react";
-import style from "./styles.module.css";
+import React, { FunctionComponent } from "react";
 
 type HightLightProps = {
   inputText: string;
   str: string;
 };
-// any
 const HightLight: FunctionComponent<HightLightProps> = ({
   inputText,
   str,
 }: HightLightProps): JSX.Element => {
   // const { inputText, str } = props;
+  const generateKey = (): string => {
+    let k = new Date().getTime() * Math.floor(Math.random() * 1000);
+    // console.log(k);
+    return `${k}`;
+  };
   if (!inputText) return <>{str}</>;
   const regexp: RegExp = new RegExp(inputText, "ig");
-  console.log(regexp);
+  // console.log(regexp);
   const matchValue: RegExpMatchArray | null = str.match(regexp);
-  console.log(matchValue);
+  // console.log(matchValue);
   if (matchValue) {
     let strSplit: string[] = str.split(regexp);
     let strSplitMap: JSX.Element[] = strSplit.map(
       (s: string, index: number, array: string[]) => {
         if (index < array.length - 1) {
           const c: string | undefined = matchValue.shift();
-          console.log(c);
+          // console.log(c);
           return (
-            <>
+            <React.Fragment key={generateKey()}>
               {s}
               <span className={"hightlight"}>{c}</span>
-            </>
+            </React.Fragment>
           );
         }
-        return <>{s}</>;
+        return <React.Fragment key={generateKey()}>{s}</React.Fragment>;
       }
     );
+    // console.log(strSplitMap);
     let wrap: JSX.Element = <>{strSplitMap}</>;
     return wrap;
   }
